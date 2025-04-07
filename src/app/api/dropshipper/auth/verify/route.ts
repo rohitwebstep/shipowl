266fd6c94e94bf6c97a6f3cd883bf8a50a71c5f5
rohitwebstep <rohitwebstep@gmail.com>
2025-04-07 12:01:35 +0530

    fix: resolved post-deployment issues and bugs

--- src/app/api/dropshipper/auth/verify/route.ts
+++ src/app/api/dropshipper/auth/verify/route.ts
@@ -1,5 +1,5 @@
 import { handleVerifyLogin } from '../../../controllers/admin/authController';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
     const adminRole = "dropshipper";
