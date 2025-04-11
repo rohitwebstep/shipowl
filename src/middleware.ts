deb7c80785c6607253e5e3042ea9166167d124f3
rohitwebstep <rohitwebstep@gmail.com>
2025-04-11 13:21:51 +0530

    chore(api): allow all HTTP methods for route handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,8 @@ export function middleware(req: NextRequest) {
     const res = NextResponse.next();
     // Apply CORS headers globally
     res.headers.set("Access-Control-Allow-Origin", "*");  // Change '*' to your frontend URL in production
-    res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    // res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.headers.set("Access-Control-Allow-Methods", "*");
     res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     console.log(`req.method: ${req.method}`);
