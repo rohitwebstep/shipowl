f2209877476bf237af8b11ee20672a2fcb9e46a3
rohitwebstep <rohitwebstep@gmail.com>
2025-04-11 11:29:24 +0530

    try to resolve deployment errors

--- prisma/prisma/generated/client/edge.js
+++ prisma/prisma/generated/client/edge.js
@@ -259,11 +259,12 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "mysql://root:@localhost:3306/shippingowl"
+        "value": null
       }
     }
   },
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/prisma/generated/client"; // Adjust the import path as necessary
 
 const prisma = new PrismaClient();
 
