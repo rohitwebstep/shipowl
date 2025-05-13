f5a8100cac9c0f807b2a6ddc69a234331c0715ce
rohitwebstep <rohitwebstep@gmail.com>
2025-05-13 14:09:21 +0530

    Created Order Cancel API to handle order cancellation requests via AWB number

---
+++ .next/server/app/api/order/[orderId]/shipping/cancel/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/order/[orderId]/shipping/cancel/route.js";
+const runtime = require("../../../../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/[root of the server]__edf79db4._.js");
+runtime.loadChunk("server/chunks/node_modules_33e63d7f._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/order/[orderId]/shipping/cancel/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/order/[orderId]/shipping/cancel/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/order/[orderId]/shipping/cancel/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
