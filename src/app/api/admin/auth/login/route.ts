76a9ae06c515a2b89823462a95bf5ed14ac354ba
rohitwebstep <rohitwebstep@gmail.com>
2025-05-14 13:19:53 +0530

    fix: try to resolve deployment errors - 3

--- src/app/api/admin/auth/login/route.ts
+++ src/app/api/admin/auth/login/route.ts
@@ -1,9 +1,21 @@
-import { checkAdminPermission } from '@/utils/auth/checkAdminPermission';
+// import { checkAdminPermission } from '@/utils/auth/checkAdminPermission';
 import { handleLogin } from '../../../controllers/admin/authController';
 import { NextRequest } from 'next/server';
 
 export async function POST(req: NextRequest) {
     const adminRole = "admin";
     const adminStaffRole = "admin_staff";
+
+    /*
+    const result = await checkAdminPermission({
+        admin_id: 1,
+        admin_type: "main",
+        panel: "admin",
+        role: "admin",
+        module: "admin",
+        action: "create"
+    });
+    */
+
     return handleLogin(req, adminRole, adminStaffRole);
 }
