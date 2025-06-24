073c65257170193a4c46956647d06e818ce5d10b
rohitwebstep <rohitwebstep@gmail.com>
2025-06-24 14:05:29 +0530

    fix: try to resolve deployment errors - 5

--- .next/server/chunks/[root of the server]__77440f68._.js
+++ .next/server/chunks/[root of the server]__77440f68._.js
@@ -11326,7 +11326,6 @@ async function GET(req) {
             }
         }
         const supplierPermissionsResult = await (0, __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$models$2f$supplier$2f$order$2f$permission$2e$ts__$5b$app$2d$route$5d$__$28$ecmascript$29$__["getPermissions"])();
-        console.log(`rtoDeliveredDate - `, orders[0].rtoDeliveredDate);
         return __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$app$2d$route$5d$__$28$ecmascript$29$__["NextResponse"].json({
             status: true,
             reportAnalytics,
